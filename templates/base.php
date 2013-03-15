dba3fce77edf63d7155e247ce9963412b9c10ecb
Ludi <hello@ludi.be>
2013-03-15 21:13:17 +0100

    lien About sur name

--- templates/base.php
+++ templates/base.php
@@ -47,7 +47,7 @@ if($is_logged)
 </div>
 <div id="header">
  <div id="name-box">
-        <a class="name-link" href="http://emilie.seron.local/at/About">Emilie Seron</a>
+        <a class="name-link" href="http://emilieseron.com/at/About">Emilie Seron</a>
     </div>
    
     <div id="titre-box"> 
